2b9e56a08204a942e730b9745f61db53a1aef574
Kerwin Lim <kerwinlim25@yahoo.com>
2018-05-27 22:04:36 -0400

    1st commit

---
+++ app_server/controllers/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by kerwi on 5/27/2018.
+ */
+
+/* GET home page. */
+module.exports.index = function (req, res){
+    res.render('index', { title: 'Express!!! what the Fuck!!!' });
+};
+
